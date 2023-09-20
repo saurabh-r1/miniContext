277dfff45795444a2618a8bb8280a73cba7b07eb
saurabh-r1 <sidlock2@gmail.com>
2023-09-20 20:11:39 +0530

    mini context completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const counting =()=>{
-    setCount((count) => count + 1)
-  }
 
   return (
     <>
-      <div className="card">
-        <button onClick={counting}>
-          count is {count}
-        </button>
-      </div>
+      <UserContextProvider>
+        <h1>Hello</h1>
+        <Login />
+        <Profile />
+      </UserContextProvider>
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
         <input
         value={username}
         onChange={(e)=> setUsername(e.target.value)}
-         type='text' placeholder='username' />s
+         type='text' placeholder='username' /> 
         <input
          value={password}
          onChange={(e)=> setPassword(e.target.value)}
